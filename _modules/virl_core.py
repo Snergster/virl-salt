606f52cf70a7e098ff0e901ca492d20be3039073
virl <virl@virl-2-host-07.cisco.com>
2014-10-02 13:31:21 -0700

    moving modules and states here

---
+++ _modules/virl_core.py
@@ -0,0 +1,320 @@
+"""
+VIRL-CORE salt execution module.
+
+Provides functions for STD and OpenStack user and project management.
+Uses virl_uwm_client and virl_openstack_client commands.
+
+To use this module on a minion::
+
+  - place it into /srv/salt/_modules/
+    and call: `sudo salt-call --local saltutil.sync_modules`
+    then: `sudo salt-call --local virl_core.<function> <parameters>`
+
+  - or directly pass the module's location with the -m parameter to salt-call:
+    `sudo salt-call --local -m <path> virl_core.<function> <parameters>`
+"""
+
+
+import simplejson
+import subprocess
+from salt.exceptions import CommandExecutionError
+
+
+# Default parameter values
+VIRL_NEEDS_SUDO = True
+uwm_username = 'uwmadmin'
+uwmadmin_password = 'password'
+uwm_url = 'http://localhost:19400'
+os_username = 'admin'
+password = 'password'
+OS_TENANT = 'admin'
+OS_AUTH_URL = 'http://localhost:5000/v2.0'
+
+
+def __virtual__():
+    """Return the name of this execution module"""
+    # return False if should not be loaded
+    return 'virl_core'
+
+
+def __get_config(key, default=None):
+    """Get salt configuration using config.get, defaulting to `default`"""
+    prefix = 'virl_core.'
+    return __salt__['config.get'](key, default)
+
+
+class VirlCommandExecutionError(CommandExecutionError):
+
+    def __init__(self, output, exitcode=None):
+        super(VirlCommandExecutionError, self).__init__(output, exitcode)
+        self.output = output
+        self.exitcode = exitcode
+
+
+def __command(command, subcommand, args=(), kwargs={}, subargs=(),
+              subkwargs={}):
+    """Always returns a dict parsed from the output JSON, or
+    raises a `VirlCommandExecutionError` with stdout and stderr and exit code.
+
+    """
+    cmd = []
+    if __get_config('virl_needs_sudo', VIRL_NEEDS_SUDO):
+        cmd.append('sudo')
+    cmd.append(command)
+    cmd.extend(args)
+    for item in kwargs.iteritems():
+        cmd.extend(item)
+    cmd.append(subcommand)
+    cmd.extend(subargs)
+    for item in subkwargs.iteritems():
+        cmd.extend(item)
+    cmd = map(str, cmd)
+
+    prc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = prc.communicate()
+    code = prc.returncode
+
+    if prc.returncode == 0:
+        result = out.strip() or '{}'
+        try:
+            result = simplejson.loads(result)
+            return result
+        except Exception:
+            raise VirlCommandExecutionError(out, code)
+    else:
+        raise VirlCommandExecutionError(out + err, code)
+
+
+def __uwm_client(command, args=(), kwargs={}):
+    """Call a virl_uwm_client command"""
+    return __command(command='virl_uwm_client',
+                     args=('--quiet', '--json'),
+                     kwargs={'-l': __get_config('uwm_url', uwm_url),
+                             '-u': __get_config('uwm_username', uwm_username),
+                             '-p': __get_config('uwm_password', uwmadmin_password)},
+                     subcommand=command,
+                     subargs=args,
+                     subkwargs=kwargs)
+
+
+def __os_client(command, args=(), kwargs={}):
+    """Call a virl_openstack_client command"""
+    return __command(command='virl_openstack_client',
+                     args=('--quiet', '--json'),
+                     kwargs={'-A': __get_config('os_auth_url', OS_AUTH_URL),
+                             '-U': __get_config('os_username', os_username),
+                             '-P': __get_config('os_password', password),
+                             '-T': __get_config('os_tenant', OS_TENANT)},
+                     subcommand=command,
+                     subargs=args,
+                     subkwargs=kwargs)
+
+
+def project_list():
+    """Get all UWM projects.
+
+    :returns: list of dict
+
+    """
+    result = __uwm_client('project-info')
+    return result['projects']
+
+
+def user_list():
+    """Get all UWM users.
+
+    :returns: list of dict
+
+    """
+    result = __uwm_client('user-info')
+    return result['users']
+
+
+def project_get(name):
+    """Get a UWM project.
+
+    :returns: dict or None
+
+    """
+    try:
+        result = __uwm_client('project-info', kwargs={'-n': name})
+        return result['project']
+    except VirlCommandExecutionError as exc:
+        if '404' in exc.output:
+            return None
+        else:
+            raise
+
+
+def _tenant_get(name):
+    """Get an OpenStack project, i.e. tenant.
+
+    :returns: dict or None
+
+    """
+    try:
+        result = __os_client('identity-tenant-info',
+                             kwargs={'-f': 'name=%s' % name})
+        if result:
+            return result
+        else:
+            return None
+    except VirlCommandExecutionError as exc:
+        if '404' in exc.output:
+            return None
+        else:
+            raise
+
+
+def user_get(name):
+    """Get a UWM user.
+
+    :returns: dict or None
+
+    """
+    try:
+        result = __uwm_client('user-info', kwargs={'-n': name})
+        return result['user']
+    except VirlCommandExecutionError as exc:
+        if '404' in exc.output:
+            return None
+        else:
+            raise
+
+
+def _os_user_get(name):
+    """Get an OpenStack user.
+
+    :returns: dict or None
+
+    """
+    try:
+        result = __os_client('identity-user-info',
+                             kwargs={'-f': 'name=%s' % name})
+        if result:
+            return result
+        else:
+            return None
+    except VirlCommandExecutionError as exc:
+        if '404' in exc.output:
+            return None
+        else:
+            raise
+
+
+def project_absent(name, clear_openstack=False):
+    """Ensure that there's no project `name` in UWM. If `clear_openstack` is
+    True, will also delete OpenStack tenants not known to UWM.
+
+    :returns: dict with keys ('project', 'deleted-users', 'tenant', 'comment')
+
+    """
+    comment = 'Project did not exist'
+    project = project_get(name)
+    deleted_users = []
+    tenant = _tenant_get(name)
+
+    if project is not None:
+        result = __uwm_client('project-delete', kwargs={'-n': name})
+        if not result.get('deleted-project'):
+            raise CommandExecutionError(str(result))
+        deleted_users = result['deleted-users']
+        comment = 'UWM project was deleted'
+
+    elif tenant is not None:
+        if not clear_openstack:
+            raise CommandExecutionError('Project exists only in OpenStack, but'
+                                        ' "clear_openstack" flag is not set')
+        __os_client('identity-tenant-delete', kwargs={'-t': tenant['id']})
+        comment = 'OpenStack tenant was deleted'
+
+    return {'project': project,
+            'deleted-users': deleted_users,
+            'tenant': tenant,
+            'comment': comment}
+
+
+def user_absent(name, clear_openstack=False):
+    """Ensure that there's no user `name` in UWM. If `clear_openstack` is
+    True, will also delete OpenStack users not known to UWM.
+
+    :returns: dict with keys ('user', 'os-user', 'comment')
+
+    """
+    comment = 'User did not exist'
+    user = user_get(name)
+    os_user = _os_user_get(name)
+
+    if user is not None:
+        __uwm_client('user-delete', kwargs={'-n': name})
+        comment = 'UWM user was deleted'
+
+    elif os_user is not None:
+        if not clear_openstack:
+            raise CommandExecutionError('User exists only in OpenStack, but'
+                                        ' "clear_openstack" flag is not set')
+        __os_client('identity-user-delete', kwargs={'-u': os_user['id']})
+        comment = 'OpenStack user was deleted'
+
+    return {'user': user,
+            'os-user': os_user,
+            'comment': comment}
+
+
+def project_present(name, **kwargs):
+    """Ensure that there's a UWM project with the specified properties.
+
+    :returns: dict with keys ('project', 'main-user', 'comment')
+
+    """
+    props = {'name': name}
+    for key in ('description', 'expires', 'enabled', 'user_password',
+                'user_os_password', 'networks', 'quota_instances', 'quota_ram',
+                'quota_vcpus'):
+        if key in kwargs:
+            json_key = key.replace('_', '-')
+            props[json_key] = kwargs[key]
+    data = simplejson.dumps({'projects': [props],
+                             'users': []})
+
+    results = __uwm_client('project-import',
+                           args=('--update',),
+                           kwargs={'--data': data})
+
+    result = results['results'][0]
+    if 'exception' in result:
+        raise CommandExecutionError(result['exception'])
+
+    return result
+
+
+def user_present(name, password, project, role, **kwargs):
+    """Ensure that there's a UWM user with the specified properties.
+
+    :returns: dict with keys ('user', 'comment')
+
+    """
+    if project_get(project) is None:
+        raise CommandExecutionError('Project does not exist')
+
+    props = {'username': name,
+             'password': password,
+             'project': project,
+             'role': role}
+    for key in ('os_password', 'email', 'expires', 'enabled', 'endpoint',
+                'ssh_public_key'):
+        if key in kwargs:
+            json_key = key.replace('_', '-')
+            props[json_key] = kwargs[key]
+    data = simplejson.dumps({'projects': [],
+                             'users': [props]})
+
+    results = __uwm_client('project-import',
+                           args=('--update',),
+                           kwargs={'--data': data})
+
+    result = results['results'][0]
+    if 'exception' in result:
+        raise CommandExecutionError(result['exception'])
+
+    return result
---
+++ _states/virl_core.py
@@ -0,0 +1,173 @@
+"""
+VIRL-CORE salt state module.
+
+Place this file on the minion under /srv/salt/_states/
+and load it with: `sudo salt-call --local saltutil.sync_states`
+
+To apply an SLS file, place it into /srv/salt/ then call:
+`sudo salt-call --local state.sls <filename>`
+
+An example SLS:
+
+.. code-block:: yaml
+
+    proj123:
+      virl_core.project_present:
+        - description: First project
+        - expires: 2015-09-30 23:59
+        - quota_instances: 50
+        - quota_ram: 256000
+        - quota_vcpus: 100
+
+    proj123 main user:
+      virl_core.user_present:
+        - name: proj123
+        - password: secret
+        - project: proj123
+        - role: admin
+        - require:
+          - virl_core: proj123
+
+    proj123-user:
+      virl_core.user_present:
+        - password: secret
+        - project: proj123
+        - role: _member_
+        - enabled: False
+        - require:
+          - virl_core: proj123
+
+    testing:
+      virl_core.project_absent:
+        - clear_openstack: True
+"""
+
+
+def __virtual__():
+    """Return the name of this state module"""
+    # return False if should not be loaded
+    return 'virl_core'
+
+
+def __get_function(name):
+    """Get a function of the virl_core module"""
+    prefix = 'virl_core.'
+    return __salt__[prefix + name]
+
+
+def project_present(name, **kwargs):
+    """Ensure that the UWM project `name` and its main user exist with the
+    specified properties
+
+    """
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'comment': ''}
+
+    try:
+        project = __get_function('project_get')(name)
+
+        main_user = None
+        if project and name in project['users']:
+            main_user = __get_function('user_get')(name)
+
+        result = __get_function('project_present')(name=name, **kwargs)
+
+    except Exception as exc:
+        ret['result'] = False
+        ret['comment'] = str(exc)
+    else:
+        ret['changes']['project'] = {'old': project,
+                                     'new': result['project']}
+        ret['changes']['main-user'] = {'old': main_user,
+                                       'new': result['main-user']}
+        ret['comment'] = result['comment']
+
+    return ret
+
+
+def project_absent(name, clear_openstack=False):
+    """Ensure that there's no project `name` in UWM. If `clear_openstack` is
+    True, will also delete OpenStack tenants not known to UWM.
+
+    """
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'comment': ''}
+
+    try:
+        fun = __get_function('project_absent')
+        result = fun(name, clear_openstack=clear_openstack)
+
+    except Exception as exc:
+        ret['result'] = False
+        ret['comment'] = str(exc)
+    else:
+        project = result['project']
+        del_users = result.get('deleted-users') or []
+        tenant = result.get('tenant')
+        if project is not None:
+            ret['changes']['project'] = {'old': project, 'new': None}
+            all_users = project['users']
+            users_left = list(set(all_users) - set(del_users))
+            ret['changes']['project-users'] = {'old': all_users,
+                                               'new': users_left}
+        elif tenant is not None:
+            ret['changes']['tenant'] = {'old': tenant, 'new': None}
+        ret['comment'] = result['comment']
+
+    return ret
+
+
+def user_present(name, password, project, role, **kwargs):
+    """Ensure that the UWM user `name` exists with the specified properties"""
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'comment': ''}
+
+    try:
+        user = __get_function('user_get')(name)
+        result = __get_function('user_present')(name=name, password=password,
+                                                project=project, role=role,
+                                                **kwargs)
+    except Exception as exc:
+        ret['result'] = False
+        ret['comment'] = str(exc)
+    else:
+        ret['changes']['user'] = {'old': user,
+                                  'new': result['user']}
+        ret['comment'] = result['comment']
+
+    return ret
+
+
+def user_absent(name, clear_openstack=False):
+    """Ensure that there's no user `name` in UWM. If `clear_openstack` is
+    True, will also delete OpenStack users not known to UWM.
+
+    """
+    ret = {'name': name,
+           'result': True,
+           'changes': {},
+           'comment': ''}
+
+    try:
+        fun = __get_function('user_absent')
+        result = fun(name, clear_openstack=clear_openstack)
+
+    except Exception as exc:
+        ret['result'] = False
+        ret['comment'] = str(exc)
+    else:
+        user = result['user']
+        os_user = result.get('os-user')
+        if user is not None:
+            ret['changes']['user'] = {'old': user, 'new': None}
+        elif os_user is not None:
+            ret['changes']['os_user'] = {'old': os_user, 'new': None}
+        ret['comment'] = result['comment']
+
+    return ret
