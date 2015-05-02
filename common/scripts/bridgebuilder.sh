623a39042a4852a7e7768161e7482a42e2d46daa
Ed Kern <ejkern@mac.com>
2015-05-02 13:46:42 -0700

    breakup building new bridges for fewer state failures

---
+++ common/scripts/bridgebuilder.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+mkdir /tmp/bridge
+cd /tmp/bridge/
+version=`uname -r`
+apt-get install -y linux-headers-$version
+apt-get source -y linux-image-$version
+cd linux*/
+sed -i -e '/BR_GROUPFWD_RESTRICTED/s/4007/0000/' net/bridge/br_private.h
+cp /usr/src/linux-headers-$version/Module.symvers .
+make olddefconfig
+make prepare modules_prepare
+make SUBDIRS=scripts/mod
+make SUBDIRS=net/bridge modules
+cp -f net/bridge/bridge.ko /lib/modules/$version/kernel/net/bridge/
