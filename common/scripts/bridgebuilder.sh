36c0acb4ea64925534f94aa9896ea0a84aaad1ea
Ed Kern <ejkern@mac.com>
2016-02-05 08:24:47 -0700

    revert hack

--- common/scripts/bridgebuilder.sh
+++ common/scripts/bridgebuilder.sh
@@ -8,7 +8,6 @@ apt-get install -y linux-headers-$version
 apt-get source -y linux-image-$version
 cd linux*/
 sed -i -e '/BR_GROUPFWD_RESTRICTED/s/4007/0000/' net/bridge/br_private.h
-sed -i -e '/BR_GROUPFWD_RESTRICTED/s/0007/0000/' net/bridge/br_private.h
 cp /usr/src/linux-headers-$version/Module.symvers .
 make olddefconfig
 make prepare modules_prepare
