bbfaf63aa7822cfab901f45c0f1bf0da2cfd4741
movciari <movciari@cisco.com>
2017-05-22 08:35:08 +0000

    VIRLDEV-5320, recursive -> iterative

--- virl/files/virl_setup.py
+++ virl/files/virl_setup.py
@@ -58,6 +58,10 @@ LOG_PATHS = [
 UKSM_KERNEL_PATH = '/sys/kernel/mm/uksm/run'
 
 
+class InvalidState(Exception):
+    pass
+
+
 class Config(object):
     """ Handler for configuration files """
 
@@ -125,7 +129,7 @@ def ask_if_permanent():
     print('Make this change permanent ? y/N')
     inp = str(raw_input()).lower() or 'n'
     if inp not in {'y', 'n'}:
-        fsm('2.4', unknown_state=True)
+        raise InvalidState
     return inp == 'y'
 
 
@@ -167,7 +171,7 @@ def press_return_to_continue(next_state=''):
     print('')
     print('press return to continue')
     raw_input()
-    fsm(next_state)
+    return next_state
 
 
 def read_next_state(previous_state, default='0'):
@@ -180,10 +184,12 @@ def read_next_state(previous_state, default='0'):
     else:
         next_state = '{}'.format(inp)
 
+    print('next_state {}'.format(next_state))
+
     if next_state in STATES:
-        fsm(next_state)
+        return next_state
     else:
-        fsm(previous_state, True)
+        raise InvalidState
 
 
 def restart_docker():
@@ -218,7 +224,7 @@ def handle_start():
     print('3. Diagnostic')
     print('')
     print('0. Exit')
-    read_next_state(current_state)
+    return read_next_state(current_state)
 
 
 def handle_0():
@@ -236,7 +242,7 @@ def handle_1():
     print('5. NTP server')
     print('')
     print('0. Back')
-    read_next_state(current_state)
+    return read_next_state(current_state)
 
 
 def handle_1_1():
@@ -248,7 +254,7 @@ def handle_1_1():
     )
     config.write()
 
-    press_return_to_continue('1')
+    return press_return_to_continue('1')
 
 
 def handle_1_2():
@@ -261,7 +267,7 @@ def handle_1_2():
     run_salt_state('virl.host')
     run_salt_state('virl.network.int')
 
-    press_return_to_continue('1')
+    return press_return_to_continue('1')
 
 
 def handle_1_3():
@@ -298,7 +304,7 @@ def handle_1_3():
     run_salt_state('virl.host')
     run_salt_state('virl.network.int')
 
-    press_return_to_continue('1')
+    return press_return_to_continue('1')
 
 
 def handle_1_4():
@@ -320,7 +326,7 @@ def handle_1_4():
     run_salt_state('virl.network.int')
     run_salt_state('virl.host')
 
-    press_return_to_continue('1')
+    return press_return_to_continue('1')
 
 
 def handle_1_5():
@@ -334,7 +340,7 @@ def handle_1_5():
     run_command('sudo vinstall salt')
     run_salt_state('virl.vinstall')
     run_salt_state('virl.ntp')
-    press_return_to_continue('1')
+    return press_return_to_continue('1')
 
 
 def handle_2():
@@ -353,7 +359,7 @@ def handle_2():
         print('4. ! A UKSM-enabled kernel is not running')
     print('')
     print('0. Back')
-    read_next_state(current_state)
+    return read_next_state(current_state)
 
 
 def handle_2_1():
@@ -368,7 +374,7 @@ def handle_2_1():
     print('6. Docker registry')
     print('')
     print('0. Back')
-    read_next_state(current_state)
+    return read_next_state(current_state)
 
 
 def handle_2_1_1():
@@ -376,7 +382,7 @@ def handle_2_1_1():
     for service in VIRL_SERVICES:
         restart_service(service)
     restart_docker()
-    press_return_to_continue('2.1')
+    return press_return_to_continue('2.1')
 
 
 def handle_2_1_2():
@@ -385,20 +391,20 @@ def handle_2_1_2():
     restart_service('virl-tap-counter.service')
     restart_service('redis.service')
     restart_service('redis-server.service')
-    press_return_to_continue('2.1')
+    return press_return_to_continue('2.1')
 
 
 def handle_2_1_3():
     print('***** Restarting UWM services ******')
     restart_service('virl-uwm.service')
     restart_service('virl-webmux.service')
-    press_return_to_continue('2.1')
+    return press_return_to_continue('2.1')
 
 
 def handle_2_1_4():
     print('***** Restarting ANK services *****')
     restart_service('ank-cisco-webserver.service')
-    press_return_to_continue('2.1')
+    return press_return_to_continue('2.1')
 
 
 def handle_2_1_5():
@@ -406,14 +412,14 @@ def handle_2_1_5():
     restart_service('virl-vis-mux.service')
     restart_service('virl-vis-processor.service')
     restart_service('virl-vis-webserver.service')
-    press_return_to_continue('2.1')
+    return press_return_to_continue('2.1')
 
 
 def handle_2_1_6():
     print('***** Restarting docker registry *****')
     print('')
     restart_docker()
-    press_return_to_continue('2.1')
+    return press_return_to_continue('2.1')
 
 
 def handle_2_2():
@@ -428,14 +434,14 @@ def handle_2_2():
     print('6. Infrastructure services (MySql, RabbitMQ)')
     print('')
     print('0. Back')
-    read_next_state(current_state)
+    return read_next_state(current_state)
 
 
 def handle_2_2_1():
     print('***** Restarting OpenStack services *****')
     for service in OPENSTACK_SERVICES:
         restart_service(service)
-    press_return_to_continue('2.2')
+    return press_return_to_continue('2.2')
 
 
 def handle_2_2_2():
@@ -447,7 +453,7 @@ def handle_2_2_2():
     restart_service('nova-conductor.service')
     restart_service('nova-novncproxy.service')
     restart_service('nova-serialproxy.service')
-    press_return_to_continue('2.2')
+    return press_return_to_continue('2.2')
 
 
 def handle_2_2_3():
@@ -459,27 +465,27 @@ def handle_2_2_3():
     restart_service('neutron-metadata-agent.service')
     restart_service('neutron-linuxbridge-agent.service')
     restart_service('neutron-ovs-cleanup.service')
-    press_return_to_continue('2.2')
+    return press_return_to_continue('2.2')
 
 
 def handle_2_2_4():
     print('***** Restarting OpenStack Image services *****')
     restart_service('glance-api.service')
     restart_service('glance-registry.service')
-    press_return_to_continue('2.2')
+    return press_return_to_continue('2.2')
 
 
 def handle_2_2_5():
     print('***** Restarting OpenStack Identity services *****')
     restart_service('keystone.service')
-    press_return_to_continue('2.2')
+    return press_return_to_continue('2.2')
 
 
 def handle_2_2_6():
     print('***** Restarting OpenStack Infrastructure services *****')
     restart_service('mysql.service ')
     restart_service('rabbitmq-server.service')
-    press_return_to_continue('2.2')
+    return press_return_to_continue('2.2')
 
 
 def handle_2_3():
@@ -489,7 +495,7 @@ def handle_2_3():
     print('')
     print('Restarting OpenStack workers')
     run_salt_state('openstack.worker_pool')
-    press_return_to_continue('2')
+    return press_return_to_continue('2')
 
 
 def handle_2_4():
@@ -504,7 +510,7 @@ def handle_2_4():
             run_command('echo 0 > /sys/kernel/mm/ksm/run')
             if ask_if_permanent():
                 run_command('sed -i \'s|echo 1> /sys/kernel/mm/ksm/run|echo 0> /sys/kernel/mm/ksm/run|\' /etc/rc.local')
-    fsm('2')
+    return('2')
 
 
 def handle_3():
@@ -517,28 +523,28 @@ def handle_3():
     print('4. Collect logs')
     print('')
     print('0. Back')
-    read_next_state(current_state)
+    return read_next_state(current_state)
 
 
 def handle_3_1():
     print('***** VIRL services status *****')
     for service in VIRL_SERVICES:
         show_status(service)
-    press_return_to_continue('3')
+    return press_return_to_continue('3')
 
 
 def handle_3_2():
     print('***** OpenStack services status *****')
     for service in OPENSTACK_SERVICES:
         show_status(service)
-    press_return_to_continue('3')
+    return press_return_to_continue('3')
 
 
 def handle_3_3():
     print('***** Health check *****')
     print('')
     run_command('virl_health_status')
-    press_return_to_continue('3')
+    return press_return_to_continue('3')
 
 
 def handle_3_4():
@@ -554,23 +560,34 @@ def handle_3_4():
     output_path = '/var/log/virl_logs/'
     run_command('tar -zcvf {}{}.tgz {} 1>/dev/null 2>/dev/null'.format(output_path, output_file, files))
     print('logs are in {}{}.tgz'.format(output_path, output_file))
-    press_return_to_continue('3')
+    return press_return_to_continue('3')
 
 
 def fsm(state, unknown_state=False):
     global STATES
+    current_state = ''
+    next_state = ''
     subprocess.call('clear')
-    if unknown_state:
-        print('Unknown command !')
-        print('')
-    else:
-        print('')
-        print('')
-    STATES[state]()
+    print('')
+    print('')
+
+    while True:
+        try:
+            current_state = next_state
+            next_state = STATES[current_state]()
+        except InvalidState:
+            next_state = current_state
+            subprocess.call('clear')
+            print('Invalid command !')
+            print('')
+        else:
+            subprocess.call('clear')
+            print('')
+            print('')
 
 
 def sigint_handler(signal, frame):
-    print('Ctrl+C detected, exit.')
+    print('\nCtrl+C detected, exit.')
     sys.exit()
 
 
